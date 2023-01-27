ceeea8802caa6958aeb6011eba59898c08a9668d
Dean Evans <evs_bud@hotmail.com>
2023-01-27 17:26:51 +0000

    first phase testing shows good.  Needs more testing and tidy up

--- cnl/cnl_analysis.py
+++ cnl/cnl_analysis.py
@@ -7,6 +7,8 @@ Setup the directories and merge keys
 
 from pathlib import Path
 import pandas as pd
+import numpy as np
+from datetime import datetime
 import sys
 import os
 
@@ -16,16 +18,124 @@ pd.options.display.width = 1000
 
 # wdir = Path(r'C:\Users\212628255\Documents\2 GE\AssetPlus\Monthly Reports\CNL')
 # fname = '20221219 - PM DATA FROM 2016 - ACTIVE.csv'
-
-path_to_file = '/home/dean/Documents/Work/CNL analysis/20221219 - PM DATA FROM 2017.csv'
-# filename = '20221219 - PM DATA FROM 2017.csv'
-# path = os.path.join('home', 'dean', 'Documents','Work','CNL analysis','20221219 - PM DATA FROM 2017.csv')
+path_to_file = '/home/dean/Documents/Work/CNL analysis/PM WO SINCE 2017.csv'
 
 
-# load data
-df_pms = pd.read_csv(path_to_file, on_bad_lines='skip')
-print(df_pms.head())
-print(df_pms.describe())
+def main():
+    # load data
+    df = pd.read_csv(path_to_file, on_bad_lines='skip')
+
+    df = df.rename(
+        columns={'WO_NUM': 'wo', 'ASSET_ID': 'asset', 'DESCRIPTION': 'name', 'SERIAL': 'serial', 'MODEL': 'model',
+                 'MANUFACTURER': 'manufacturer', 'GMDN_NO': 'gmdn', 'EQ_NUM': 'equip', 'DEPT': 'dept', 'SITE': 'site',
+                 'CALL DATE': 'call_date', 'END DATE': 'end_date', 'TECH_DEPT': 'tech_dept', 'TECHNICIAN': 'tech',
+                 'WO_SUB-STATUS': 'status', 'RISK': 'risk', 'INSTALL': 'install'})
+
+    df.drop(columns=['WO_TYPE', 'WO_SUB-TYPE'], inplace=True)
+
+    # clean and filter
+    df['wo'] = df['wo'].astype('str')
+    df['asset'] = df['asset'].astype('str')
+    df.risk = df.risk.apply(convert_risk)
+    df['status'].replace('NOT FOUND', 'COULD NOT LOCATE', inplace=True)
+    df['status'].replace('JOB DONE', 'JOB COMPLETED', inplace=True)
+    df['status'].replace('PM PASS', 'COULD NOT LOCATE', inplace=True)
+    df = df[df['status'].isin(['JOB DONE', 'JOB COMPLETED', 'PM PASS', 'COULD NOT LOCATE'])]
+
+    # make column with the first of this month
+    this_month_first = datetime(datetime.today().year, datetime.today().month, 1).date()
+    df['this_month_first'] = pd.to_datetime(this_month_first)
+
+    # make column for first of end date month
+    df['end_date_first'] = df.end_date.to_numpy().astype('datetime64[M]')
+
+    # get month difference
+    df['delta_months'] = ((df.this_month_first - df.end_date_first) / np.timedelta64(1, 'M')).astype(int)
+
+    df.sort_values('end_date', ascending=False, inplace=True)
+
+    print(df.head(20))
+    print(df['status'].value_counts())
+    print(f"Number of PMs = {len(df)}")
+
+    # group by asset so that we have all pms associated sorted from more recent to past
+    grp = df.groupby('asset')
+
+    # create empty dataframe to hold results
+    df_follow_up = pd.DataFrame()
+    df_deactivation = pd.DataFrame()
+
+    # counters
+    cnt_followup = 0
+    cnt_deactivate = 0
+    cnt_ignore = 0
+
+    for name, group in grp:
+        # reset flags
+        cnl_cnt = 0  # counts continuous cnls from first
+
+        # variables to determine last group member
+        grp_rows = len(group)
+        row_cnt = 0
+        fg_followup = False
+
+        # iterate over individuals assets jobs
+        for idx, row in group.iterrows():
+            row_cnt += 1
+            fg_first_row = row_cnt == 1
+            fg_last_row = row_cnt == grp_rows  # set last flag
+            fg_cnl = row['status'] == 'COULD NOT LOCATE'   # set cnl flag
+
+            # only look at 6 months for first row
+            if fg_first_row:
+                # reset flags
+                fg_followup = False
+
+                if row['delta_months'] == 6 and fg_cnl:
+                    # print(row)
+                    fg_followup = True
+                    cnl_cnt += 1
+                else:
+                    cnt_ignore += 1
+                    break    # get next group
+
+            # not first row but already qualified and is a cnl
+            elif fg_followup and fg_cnl:   # ALREADY CNL PLUS CNL
+                cnl_cnt += 1    # increment cnl count
+                if cnl_cnt == 3:
+                    # print(f'DEACTIVATE {row.asset}')
+                    # print(row)
+                    df_deactivation = pd.concat([df_deactivation, group], ignore_index=True)
+                    cnt_deactivate += 1
+                    break
+
+            # not cnl but the first qualified hence add to followup and terminate group
+            elif fg_followup:   # not CNL so end it here
+                # print(f'FOLLOWUP {row.asset}')
+                cnt_followup += 1
+                df_follow_up = pd.concat([df_follow_up, group], ignore_index=True)
+                break
+            # shouldn't get here
+            else:
+                print('I am illegally here')
+                break
+
+    print(f"DEACTIVATE = {cnt_deactivate}")
+    print(f"FOLLOWUP = {cnt_followup}")
+    print(f"IGNORE = {cnt_ignore}")
+
+    #         if row['status'] == 'COULD NOT LOCATE':
+    #             cnl += 1
+    #         elif cnl >= CNL_LIMIT:
+    #             df_out = pd.concat([df_out, group], ignore_index=True)
+    #             main_cnt += 1
+    #             break
+    #         else:  # not CNL and <2 hence skip asset group without saving
+    #             break
+    #
+    # print(f"Total assets = {main_cnt}")
+    df_deactivation.to_excel('DEACTIVATION.xlsx', index=False)
+    df_follow_up.to_excel('FOLLOWUP.xlsx', index=False)
 
 
 def convert_risk(r):
@@ -38,51 +148,6 @@ def convert_risk(r):
     else:
         return 'ERROR'
 
-# clean and filter
-df_pms['wo'] = df_pms['wo'].astype('str')
-df_pms['asset'] = df_pms['asset'].astype('str')
-df_pms.drop(columns=['call_date'], inplace=True)
-df_pms.risk = df_pms.risk.apply(convert_risk)
-df_pms['wo_substatus'].replace('NOT FOUND', 'COULD NOT LOCATE', inplace=True)
-df_pms['wo_substatus'].replace('JOB DONE', 'JOB COMPLETED', inplace=True)
-df_pms['wo_substatus'].replace('PM PASS', 'COULD NOT LOCATE', inplace=True)
-df_pms = df_pms[df_pms['wo_substatus'].isin(['JOB DONE', 'JOB COMPLETED', 'PM PASS', 'COULD NOT LOCATE'])]
 
-df_pms.sort_values('end_date', ascending=False, inplace=True)
-
-print(df_pms.head(20))
-print(df_pms['wo_substatus'].value_counts())
-print(f"Number of PMs = {len(df_pms)}")
-
-# group by asset so that we have all pms associated sorted from more recent to past 
-grp = df_pms.groupby('asset')
-
-CNL_LIMIT = 2   # sets the trigger to save the asset & pms to the results
-main_cnt = 0    # count for unique assets with CNL_LIMIT or more consecutive latest PMs
-
-# create empty dataframe to hold results
-df_out = pd.DataFrame()
-
-for name, group in grp:
-    # reset flags
-    cnl = 0     # counts continuous cnls from first
-    
-    # variables to determine last group member
-    grp_rows = len(group)
-    row_cnt = 0
-
-    # iterate over individuals assets jobs
-    for idx, row in group.iterrows():
-        row_cnt += 1
-        last = row_cnt == grp_rows
-        if row['wo_substatus'] == 'COULD NOT LOCATE':
-            cnl += 1
-        elif cnl >= CNL_LIMIT:
-            df_out = pd.concat([df_out, group], ignore_index=True)
-            main_cnt += 1
-            break
-        else:   # not CNL and <2 hence skip asset group without saving
-            break
-
-print(f"Total assets = {main_cnt}")
-df_out.to_excel((wdir / 'CNL_WITH_2_OR_MORE_CONSECUTIVE.xlsx'), index=False)
+if __name__ == '__main__':
+    main()
